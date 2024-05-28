b7e0345b63cd7e730240e75ce57c7d7b23e48043
Eric Barreto <ericbarreto521@gmail.com>
2024-05-27 21:53:16 -0300

    feat/jestTests: Creating controller and service unit tests

--- src/modules/product/product.controller.spec.ts
+++ src/modules/product/product.controller.spec.ts
@@ -1,19 +1,136 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { ProductController } from './product.controller';
-import { ProductModule } from './product.module';
+import { ProductService } from './product.service';
+import { ProductRepository } from 'src/repositories/prisma/products-repository';
+import { CreateProductDTO, UpdateProductDTO } from '../../dtos/product.dto';
+import { Product } from '@prisma/client';
+import { v4 as uuidv4 } from 'uuid';
 
 describe('ProductController', () => {
-  let controller: ProductController;
+  let productController: ProductController;
+  let productService: ProductService;
+  let productRepository: ProductRepository;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [ProductModule],
-    }).compile();
+  beforeEach(() => {
+    productRepository = {
+      findAll: jest.fn(),
+      create: jest.fn(),
+      read: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+      decreaseStock: jest.fn(),
+    } as any;
 
-    controller = module.get<ProductController>(ProductController);
+    productService = new ProductService(productRepository);
+    productController = new ProductController(productService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of products', async () => {
+      const result = [
+        {
+          id: uuidv4(),
+          title: 'Product 1',
+          price: 100,
+          thumbnail: 'thumbnail1.jpg',
+          category: 'Category 1',
+          stock: 10,
+          description: 'Description 1',
+          brand: 'Brand 1',
+        },
+      ];
+
+      jest.spyOn(productService, 'findAll').mockResolvedValue(result);
+
+      expect(await productController.readAll()).toBe(result);
+    });
+  });
+
+  describe('create', () => {
+    it('should create and return a product', async () => {
+      const createProductDTO: CreateProductDTO = {
+        title: 'Product 1',
+        price: 100,
+        thumbnail: 'thumbnail1.jpg',
+        category: 'Category 1',
+        stock: 10,
+        description: 'Description 1',
+        brand: 'Brand 1',
+      };
+      const result: Product = { id: uuidv4(), ...createProductDTO };
+
+      jest.spyOn(productService, 'create').mockResolvedValue(result);
+
+      expect(await productController.create(createProductDTO)).toBe(result);
+    });
+  });
+
+  describe('readOne', () => {
+    it('should return a single product', async () => {
+      const id = uuidv4();
+      const result: Product = {
+        id,
+        title: 'Product 1',
+        price: 100,
+        thumbnail: 'thumbnail1.jpg',
+        category: 'Category 1',
+        stock: 10,
+        description: 'Description 1',
+        brand: 'Brand 1',
+      };
+
+      jest.spyOn(productService, 'read').mockResolvedValue(result);
+
+      expect(await productController.readOne(id)).toBe(result);
+    });
+  });
+
+  describe('update', () => {
+    it('should update and return the updated product', async () => {
+      const id = uuidv4();
+      const updateProductDTO: UpdateProductDTO = {
+        title: 'Updated Product',
+        price: 150,
+        thumbnail: 'updated_thumbnail.jpg',
+        category: 'Updated Category',
+        stock: 5,
+        description: 'Updated Description',
+        brand: 'Updated Brand',
+      };
+      const result: Product = { id, ...updateProductDTO };
+
+      jest.spyOn(productService, 'update').mockResolvedValue(result);
+
+      expect(await productController.update(id, updateProductDTO)).toBe(result);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete the product and return undefined', async () => {
+      const id = uuidv4();
+
+      jest.spyOn(productService, 'delete').mockResolvedValue(undefined);
+
+      expect(await productController.delete(id)).toBe(undefined);
+    });
+  });
+
+  describe('decreaseStock', () => {
+    it('should decrease the stock of the product and return the updated product', async () => {
+      const id = uuidv4();
+      const result: Product = {
+        id,
+        title: 'Product 1',
+        price: 100,
+        thumbnail: 'thumbnail1.jpg',
+        category: 'Category 1',
+        stock: 9,
+        description: 'Description 1',
+        brand: 'Brand 1',
+      };
+
+      jest.spyOn(productService, 'decreaseStock').mockResolvedValue(result);
+
+      expect(await productController.decreaseStock(id)).toBe(result);
+    });
   });
 });
--- src/modules/product/product.service.spec.ts
+++ src/modules/product/product.service.spec.ts
@@ -1,19 +1,174 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Product } from '@prisma/client';
+import { CreateProductDTO, UpdateProductDTO } from 'src/dtos/product.dto';
+import { ProductRepository } from '../../repositories/prisma/products-repository';
 import { ProductService } from './product.service';
-import { ProductModule } from './product.module';
 
 describe('ProductService', () => {
   let productService: ProductService;
+  let productRepository: ProductRepository;
 
   beforeEach(async () => {
+    const mockProductRepository = {
+      create: jest.fn(),
+      findByTitle: jest.fn(),
+      read: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+      findAll: jest.fn(),
+      decreaseStock: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ProductModule],
+      providers: [
+        ProductService,
+        {
+          provide: ProductRepository,
+          useValue: mockProductRepository,
+        },
+      ],
     }).compile();
 
     productService = module.get<ProductService>(ProductService);
+    productRepository = module.get<ProductRepository>(ProductRepository);
   });
 
   it('should be defined', () => {
     expect(productService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create and return a product', async () => {
+      const createProductDTO: CreateProductDTO = {
+        title: 'Product 1',
+        price: 100,
+        thumbnail: 'thumbnail1.jpg',
+        category: 'Category 1',
+        stock: 10,
+        description: 'Description 1',
+        brand: 'Brand 1',
+      };
+      const product: Product = { id: 'uuid', ...createProductDTO };
+
+      jest.spyOn(productRepository, 'findByTitle').mockResolvedValue(null);
+      jest.spyOn(productRepository, 'create').mockResolvedValue(product);
+
+      expect(await productService.create(createProductDTO)).toBe(product);
+    });
+  });
+
+  describe('read', () => {
+    it('should return a product', async () => {
+      const product: Product = {
+        id: 'uuid',
+        title: 'Product 1',
+        price: 100,
+        thumbnail: 'thumbnail1.jpg',
+        category: 'Category 1',
+        stock: 10,
+        description: 'Description 1',
+        brand: 'Brand 1',
+      };
+
+      jest.spyOn(productRepository, 'read').mockResolvedValue(product);
+
+      expect(await productService.read('uuid')).toBe(product);
+    });
+  });
+
+  describe('update', () => {
+    it('should update and return the product', async () => {
+      const updateProductDTO: UpdateProductDTO = {
+        title: 'Updated Product',
+        price: 150,
+        thumbnail: 'updated_thumbnail.jpg',
+        category: 'Updated Category',
+        stock: 5,
+        description: 'Updated Description',
+        brand: 'Updated Brand',
+      };
+      const product: Product = { id: 'uuid', ...updateProductDTO };
+
+      jest.spyOn(productRepository, 'read').mockResolvedValue(product);
+      jest.spyOn(productRepository, 'update').mockResolvedValue(product);
+
+      expect(await productService.update('uuid', updateProductDTO)).toBe(
+        product,
+      );
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete and return the product', async () => {
+      const product: Product = {
+        id: 'uuid',
+        title: 'Product 1',
+        price: 100,
+        thumbnail: 'thumbnail1.jpg',
+        category: 'Category 1',
+        stock: 10,
+        description: 'Description 1',
+        brand: 'Brand 1',
+      };
+
+      jest.spyOn(productRepository, 'read').mockResolvedValue(product);
+      jest.spyOn(productRepository, 'delete').mockResolvedValue(product);
+
+      expect(await productService.delete('uuid')).toBe(product);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of products', async () => {
+      const products: Product[] = [
+        {
+          id: 'uuid1',
+          title: 'Product 1',
+          price: 100,
+          thumbnail: 'thumbnail1.jpg',
+          category: 'Category 1',
+          stock: 10,
+          description: 'Description 1',
+          brand: 'Brand 1',
+        },
+        {
+          id: 'uuid2',
+          title: 'Product 2',
+          price: 150,
+          thumbnail: 'thumbnail2.jpg',
+          category: 'Category 2',
+          stock: 5,
+          description: 'Description 2',
+          brand: 'Brand 2',
+        },
+      ];
+
+      jest.spyOn(productRepository, 'findAll').mockResolvedValue(products);
+
+      expect(await productService.findAll()).toBe(products);
+    });
+  });
+
+  describe('decreaseStock', () => {
+    it('should decrease the stock of the product and return the updated product', async () => {
+      const product: Product = {
+        id: 'uuid',
+        title: 'Product 1',
+        price: 100,
+        thumbnail: 'thumbnail1.jpg',
+        category: 'Category 1',
+        stock: 10,
+        description: 'Description 1',
+        brand: 'Brand 1',
+      };
+      const updatedProduct: Product = { ...product, stock: product.stock - 1 };
+
+      jest.spyOn(productRepository, 'read').mockResolvedValue(product);
+      jest
+        .spyOn(productRepository, 'decreaseStock')
+        .mockResolvedValue(updatedProduct);
+
+      expect(await productService.decreaseStock('uuid')).toBe(updatedProduct);
+    });
   });
 });
