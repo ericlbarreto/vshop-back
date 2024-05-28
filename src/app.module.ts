3f3a8726187a096fc51117422bcc820e14e44f58
Eric Barreto <ericbarreto521@gmail.com>
2024-05-27 22:05:22 -0300

    feat/jestTests: fix e2e test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './modules/product/product.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ProductModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
